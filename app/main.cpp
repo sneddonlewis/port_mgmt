c0be53cc3e89092cb6f6ce71d18f6ed0cc7d9c2a
Lewis Sneddon <lewis.sneddon@icloud.com>
2023-09-24 08:39:41 +0100

    Simple menu loop for cli

--- app/main.cpp
+++ app/main.cpp
@@ -2,6 +2,7 @@
 // doctest definitions. For builds with this disabled, e.g. code shipped to
 // users, this can be left out.
 #include <iostream>
+#include <ostream>
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
@@ -11,8 +12,44 @@
 
 void printVersionInfo();
 
+struct Position {
+    std::string ticker;
+    double quantity;
+    bool is_long;
+    double open_price;
+    std::chrono::system_clock::time_point open_timestamp;
+};
+
+auto printMenu() {
+    std::cout 
+        << "1. Add"
+        << std::endl
+        << "2. View"
+        << std::endl
+        << "0. Exit"
+        << std::endl;
+}
+
 int main() {
-    printVersionInfo();
+    int choice = -1;
+    while (choice != 0) {
+        printMenu();
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                std::cout << "Add" << std::endl;
+                break;
+            case 2:
+                std::cout << "View" << std::endl;
+                break;
+            case 0:
+                std::cout << "Bye" << std::endl;
+                break;
+            default:
+                std::cout << "Unsupported selection" << std::endl;
+        }
+    }
     return 0;
 }
 
